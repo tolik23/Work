9069abdf1d753f470943e653bd850eac384db1f8
tolik23 <tolik231081991@mail.ru>
2016-02-17 15:11:49 -0300

    Test

--- Test3/src/Main.java
+++ Test3/src/Main.java
@@ -7,10 +7,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// шаблон для регулярн выраж-ия по которому будет поиск
-		Pattern pattern = Pattern.compile("test$");
+		
+	
+		Pattern pattern = Pattern.compile("^\\d{4}\\s[A-Q]{2}-[0-7]$");
 		
 		///объект который умеет наход и сравн по задан шаблону pattern
-		Matcher matcher = pattern.matcher("gfhfghjgfh#354750646");
+		Matcher matcher = pattern.matcher("+375292741017");
 		
 		//// поиск совпадений - в ответ приход tru или false
 		if(matcher.find()) {

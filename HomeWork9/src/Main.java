33956a81628a33d8d6da4747aafe9c204544280d
tolik23 <tolik231081991@mail.ru>
2016-02-22 17:37:37 +0300

    не доделал

---
+++ HomeWork9/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Student [] students = new Student[1];
+
+        for (int i = 0; i < 1 ; i++) {
+            students[i] = Student.addStudent();
+        }
+	
+          int years =(Student() / 31536000000L);
+
+          int months =(Student() % 31536000000L) / 2592000000L );
+          
+          System.out.println("—редний возраст студентов: " +
+          years + " лет," + months + " мес€цев.");
+	
+	
+	
+	
+	}
+
+}
---
+++ HomeWork9/src/Student.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Student {
+
+    public static Student addStudent() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¬ведите им€ студента.");
+        String firstName = scanner.nextLine();
+
+        System.out.println("¬ведите фамилию студента.");
+        String SecondName = scanner.nextLine();
+
+            System.out.println("¬ведите дату рождени€.");
+            String birthDate = scanner.nextLine();
+
+    }
+}
